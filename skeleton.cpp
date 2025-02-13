f365722ae8d49a0fed5d2c5bbd05edfe467c9eed
Hemant Raj <coder.hemantraj05@gmail.com>
2025-02-13 23:08:14 +0530

    adding cpp skeleton code for cp

---
+++ skeleton.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <iomanip>
+#include <cassert>
+
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define ll long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) ((int)(v).size())
+#define F first
+#define S second
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    // Your code here
+}
+
+int main() {
+    fast_io;
+    ll t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
